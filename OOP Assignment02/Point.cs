0666d8f85f53e77b6d9d0a8d74b5cf7fcf76f4ef
Abdelrahman Nasr <abdelrhmannasr275@gmail.com>
2025-07-26 23:11:20 +0300

    Q2

---
+++ OOP Assignment02/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment02
+{
+    internal struct Point
+    {
+        public double X { set; get;} 
+        public double Y { set; get; }
+
+        public Point(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public override string ToString()
+        {
+            return $"X : {X}, Y : {Y}";
+        }
+    }
+}

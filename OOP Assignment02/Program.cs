514b5ee6bab96cd7d9657a9742776083130ba373
Abdelrahman Nasr <abdelrhmannasr275@gmail.com>
2025-08-05 20:20:39 +0300

    Completed Q1

--- OOP Assignment02/Program.cs
+++ OOP Assignment02/Program.cs
@@ -22,7 +22,19 @@ namespace OOP_Assignment02
 
         static void Main(string[] args)
         {
+            #region
+            Person[] people = new Person[3];
+            people[0] = new Person("Nasr", 23);
+            people[1] = new Person("Omar",24);
+            people[2] = new Person("Adel",22);
 
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine($"Person number {i+1} Name: {people[i].Name}");
+                Console.WriteLine($"Person number {i+1} Age: {people[i].Age}");
+            }
+
+            #endregion
 
 
             #region Q2 Create a struct called "Point" to represent a 2D point with properties "X" and "Y".Write a C# program that takes two points as input from the user and calculates the distance between them.
@@ -36,32 +48,32 @@ namespace OOP_Assignment02
 
 
             #region Q3 Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
-            Person[] people = new Person[3];
+            //Person[] people = new Person[3];
 
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine($"Enter details for person {i + 1}:");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine($"Enter details for person {i + 1}:");
 
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
+            //    Console.Write("Name: ");
+            //    string name = Console.ReadLine();
 
-                Console.Write("Age: ");
-                int age = int.Parse(Console.ReadLine());
+            //    Console.Write("Age: ");
+            //    int age = int.Parse(Console.ReadLine());
 
-                people[i] = new Person(name, age);
-                Console.WriteLine();
-            }
+            //    people[i] = new Person(name, age);
+            //    Console.WriteLine();
+            //}
 
-            Person oldest = people[0];
+            //Person oldest = people[0];
 
-            for (int i = 1; i < people.Length; i++)
-            {
-                if (people[i].Age > oldest.Age)
-                {
-                    oldest = people[i];
-                }
-            }
-            Console.WriteLine($"The oldest person is {oldest.Name}, Age: {oldest.Age}");
+            //for (int i = 1; i < people.Length; i++)
+            //{
+            //    if (people[i].Age > oldest.Age)
+            //    {
+            //        oldest = people[i];
+            //    }
+            //}
+            //Console.WriteLine($"The oldest person is {oldest.Name}, Age: {oldest.Age}");
             #endregion
         }
     }
